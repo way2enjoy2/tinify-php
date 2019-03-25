85e6bb80fb53297404f417af7a8727fc64c924da
way2enjoy2 <46526479+way2enjoy2@users.noreply.github.com>
2019-03-25 10:50:14 +0530

    Update integration.php

--- test/integration.php
+++ test/integration.php
@@ -1,16 +1,16 @@
 <?php
 
-if (!getenv("TINIFY_KEY")) {
-    exit("Set the TINIFY_KEY environment variable.\n");
+if (!getenv("WAY2ENJOY_KEY")) {
+    exit("Set the WAY2ENJOY_KEY environment variable.\n");
 }
 
 class ClientIntegrationTest extends PHPUnit_Framework_TestCase {
     static private $optimized;
 
     static public function setUpBeforeClass() {
-        \Tinify\setKey(getenv("TINIFY_KEY"));
-        \Tinify\setProxy(getenv("TINIFY_PROXY"));
-        \Tinify\validate();
+        \Way2enjoy\setKey(getenv("WAY2ENJOY_KEY"));
+        \Way2enjoy\setProxy(getenv("WAY2ENJOY_PROXY"));
+        \Way2enjoy\validate();
 
         $unoptimizedPath = __DIR__ . "/examples/voormedia.png";
         self::$optimized = \Tinify\fromFile($unoptimizedPath);
@@ -28,12 +28,12 @@ class ClientIntegrationTest extends PHPUnit_Framework_TestCase {
 
         /* width == 137 */
         $this->assertContains("\0\0\0\x89", $contents);
-        $this->assertNotContains("Copyright Voormedia", $contents);
+        $this->assertNotContains("Copyright Way2enjoy", $contents);
     }
 
     public function testShouldCompressFromUrl() {
         $path = tempnam(sys_get_temp_dir(), "tinify-php");
-        $source = \Tinify\fromUrl("https://raw.githubusercontent.com/tinify/tinify-php/master/test/examples/voormedia.png");
+        $source = \Way2enjoy\fromUrl("https://raw.githubusercontent.com/way2enjoy2/tinify-php/master/test/examples/voormedia.png");
         $source->toFile($path);
 
         $size = filesize($path);
@@ -44,11 +44,11 @@ class ClientIntegrationTest extends PHPUnit_Framework_TestCase {
 
         /* width == 137 */
         $this->assertContains("\0\0\0\x89", $contents);
-        $this->assertNotContains("Copyright Voormedia", $contents);
+        $this->assertNotContains("Copyright Way2enjoy", $contents);
     }
 
     public function testShouldResize() {
-        $path = tempnam(sys_get_temp_dir(), "tinify-php");
+        $path = tempnam(sys_get_temp_dir(), "way2enjoy-php");
         self::$optimized->resize(array("method" => "fit", "width" => 50, "height" => 20))->toFile($path);
 
         $size = filesize($path);
@@ -59,11 +59,11 @@ class ClientIntegrationTest extends PHPUnit_Framework_TestCase {
 
         /* width == 50 */
         $this->assertContains("\0\0\0\x32", $contents);
-        $this->assertNotContains("Copyright Voormedia", $contents);
+        $this->assertNotContains("Copyright Way2enjoy", $contents);
     }
 
     public function testShouldPreserveMetadata() {
-        $path = tempnam(sys_get_temp_dir(), "tinify-php");
+        $path = tempnam(sys_get_temp_dir(), "way2enjoy-php");
         self::$optimized->preserve("copyright", "creation")->toFile($path);
 
         $size = filesize($path);
@@ -74,6 +74,6 @@ class ClientIntegrationTest extends PHPUnit_Framework_TestCase {
 
         /* width == 137 */
         $this->assertContains("\0\0\0\x89", $contents);
-        $this->assertContains("Copyright Voormedia", $contents);
+        $this->assertContains("Copyright Way2enjoy", $contents);
     }
 }
