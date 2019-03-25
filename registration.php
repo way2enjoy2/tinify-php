fc9ef638fbbf454addeca497a37fa559d2e03e1d
way2enjoy2 <46526479+way2enjoy2@users.noreply.github.com>
2019-03-25 12:16:14 +0530

    this file was giving error. alrady registered

--- registration.php
+++ registration.php
@@ -2,4 +2,6 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_AdminNotification', __DIR__);
+//ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_AdminNotification', __DIR__);
+
+//this file was giving error. alrady registered while submitting new module. uncomment if u get error that its not defined
